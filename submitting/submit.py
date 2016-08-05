68c7802c4501d6c70351a95167f0bf62f97028a8
N4NU <kazukif12582552@gmail.com>
2016-08-05 18:22:47 +0900

    submit_solution作った

--- submitting/submit.py
+++ submitting/submit.py
@@ -29,15 +29,20 @@ def snapshot():
 def submit_problem():
     pass
 
-def submit_solution():
-    pass
+def submit_solution(problem_id, solution_spec):
+    r = requests.post("http://2016sv.icfpcontest.org/api/solution/submit", files=(('problem_id', str(problem_id)), ('solution_spec', solution_spec)), headers=headers)
+    time.sleep(1)
+    return json.loads(r.text)
 
-def save_problems():
+def save_problems(problems):
+    # 100回近くAPIを使ってしまうので注意
     for i in range(len(problems)):
-    problem_spec_hash = problems[i]['problem_spec_hash']
-    problem_input = blob_lookup(problem_spec_hash)
-    open('problem_inputs/problem-{0:d}.input.txt'.format(i), 'w').write(problem_input)
+        problem_spec_hash = problems[i]['problem_spec_hash']
+        problem_input = blob_lookup(problem_spec_hash)
+        open('problem_inputs/problem-{0:d}.input.txt'.format(problems[i]['problem_id']), 'w').write(problem_input)
 
+def save_json(fname, json_obj):
+    json.dump(json_obj, open(fname, 'w'), ensure_ascii=False)
 
 snapshot_hash_json = snapshot()
 
@@ -46,6 +51,8 @@ assert(snapshot_hash_json['ok'])
 snapshot_hash = snapshot_hash_json['snapshots'][-1]['snapshot_hash']
 
 snapshot_json = blob_lookup(snapshot_hash)
+
+# save_json('snapshot/' + snapshot_hash + '.json', snapshot_json)
 
 problems = snapshot_json['problems']
 
