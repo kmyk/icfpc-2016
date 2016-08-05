49f11f62c79728b17fa43449bb1e0f42cb83d342
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-08-05 15:05:46 +0900

    入力の可視化はだいたいできた

--- visualizer/a.py
+++ visualizer/a.py
@@ -8,6 +8,17 @@ class problem(object):
         self.polygons = polygons
         self.skeltons = skeltons
 
+        self.left = float('inf')
+        self.bottom = float('inf')
+        self.top = 0
+        self.right = 0
+        for polygon in self.polygons:
+            for x, y in polygon:
+                self.left   = min(self.left,   x)
+                self.right  = max(self.right,  x)
+                self.bottom = min(self.bottom, y)
+                self.top    = max(self.top,    y)
+
     @classmethod
     def input_from(cls, fh):
         # read polygons
@@ -33,38 +44,53 @@ class problem(object):
 
     def output_to(self, fh, SIZE=256):
         import cairo
-        WIDTH = 256
-        HEIGHT = 256
-        MARGIN = 28
+        WIDTH  = 512
+        HEIGHT = 512
 
+        # init
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
         ctx = cairo.Context(surface)
         ctx.scale(WIDTH, HEIGHT)
+
         # invert the coordinates
         ctx.scale(1, -1)
         ctx.translate(0, -1)
 
+        # write background
         ctx.set_source_rgba(0, 0, 0, 0) # transparent
         ctx.rectangle(0, 0, 1, 1) # entire of the surface
         ctx.fill()
 
+        # make margin
+        ctx.translate(0.1, 0.1)
+        ctx.scale(0.8, 0.8)
+
+        # write border
+        ctx.set_source_rgb(0, 0, 0) # black
+        ctx.set_line_width(0.01) # thin
+        ctx.rectangle(0, 0, 1, 1)
+        ctx.stroke()
+
+        # write polygons
         ctx.set_source_rgba(0.8, 0.4, 0.4, 0.6) # red
         ctx.set_line_width(0.01) # thin
         for polygon in self.polygons:
             x, y = polygon[-1]
-            ctx.move_to(float(x), float(y))
+            ctx.move_to(float(x - self.left), float(y - self.bottom))
             for x, y in polygon:
-                ctx.line_to(float(x), float(y))
+                ctx.line_to(float(x - self.left), float(y - self.bottom))
             ctx.fill()
 
+        # write skeltons
         ctx.set_source_rgba(0.5, 0, 0, 1) # red
         for skelton in self.skeltons:
             x, y = skelton[0]
-            ctx.move_to(float(x), float(y))
+            ctx.move_to(float(x - self.left), float(y - self.bottom))
             x, y = skelton[1]
-            ctx.line_to(float(x), float(y))
+            ctx.line_to(float(x - self.left), float(y - self.bottom))
             ctx.stroke()
 
+        # fin
         surface.write_to_png(fh.buffer)
 
 import sys
---
+++ visualizer/readme.md
@@ -0,0 +1,9 @@
+# visualizer
+
+まだ入力だけだよ
+
+## how to use
+
+```
+$ ./a.py < input.txt > output.png
+```
